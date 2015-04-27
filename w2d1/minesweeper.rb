b087094c4df6ad88eab1e3ac3e0eeb8200c4d161
Austin Gatlin <austin.e.gatlin@gmail.com>
2015-04-27 14:09:56 -0700

    display board with placed mines

--- w2d1/minesweeper.rb
+++ w2d1/minesweeper.rb
@@ -1,33 +1,51 @@
+#!/usr/bin/env ruby
+
 class Board
 
-  def initialize
-
-    @grid = Array.new(9) {Array.new(9){Tile.new}}
-
+  def initialize(player)
+    @player = player
+    @grid = Array.new(9) {Array.new(9){Tile.new(self,Board.value_generator)}}
   end
 
   def play
     start_of_game_display
+    display_board
   end
 
   private
     def display_board
-
+      @grid.each do |subary|
+        p subary.map{|x| x.value}
+      end
     end
 
     def start_of_game_display
       puts "Welcome to Mine Sweeper!"
     end
 
-
+    def self.value_generator
+       (rand(100) > 79) ? 1 : 0
+    end
 
 end
 
 class Tile
+  attr_reader :value
+  
+  def initialize(board, value)
+    @board = board
+    @value = value
+  end
+end
 
+class Player
+  def initialize(name)
+    @name = name
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  b = Board.new
+  charlie = Player.new("Charlie")
+  b = Board.new(charlie)
   b.play
 end
